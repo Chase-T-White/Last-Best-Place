74875b80b182789bf3a82286343e061e429f7422
cwhite70 <chase.t.white@hotmail.com>
2023-05-27 21:07:10 -0600

    footer update

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <p>
+        Last Best Place Brewery &copy; {new Date().getFullYear()} All Rights
+        Reserved
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
